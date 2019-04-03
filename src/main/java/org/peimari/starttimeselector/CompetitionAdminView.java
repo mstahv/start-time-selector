f7467585731de298a8bd332a2df3226c21b351c2
Matti Tahvonen <matti@vaadin.com>
2019-04-03 22:52:56 +0300

    * Separated admin view and added pw protection * int18n for public view and minor improvements

--- src/main/java/org/peimari/starttimeselector/CompetitionAdminView.java
+++ src/main/java/org/peimari/starttimeselector/CompetitionAdminView.java
@@ -1,30 +1,43 @@
 package org.peimari.starttimeselector;
 
-import javax.annotation.PostConstruct;
-
-import com.vaadin.flow.component.checkbox.CheckboxGroup;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Hr;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.router.Route;
+import org.apache.commons.codec.digest.Crypt;
 import org.peimari.starttimeselector.entities.Competition;
 import org.peimari.starttimeselector.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.vaadin.firitin.components.html.DynamicFileDownloader;
 import org.vaadin.firitin.components.orderedlayout.VHorizontalLayout;
 import org.vaadin.firitin.fields.LocalDateTimeField;
 
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.checkbox.Checkbox;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Hr;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.data.binder.Binder;
+import javax.annotation.PostConstruct;
 
-public class CompetitionAdminView extends AbstractTabView {
-	
-	@Autowired AdminService adminService;
-	@Autowired ClassesAndClassGroupsEditor classesAndClassGroupsEditor;
+@Route
+@Push
+public class CompetitionAdminView extends VerticalLayout {
+
+    @Autowired
+    AdminService adminService;
+    @Autowired
+    ClassesAndClassGroupsEditor classesAndClassGroupsEditor;
+
+    @Value("${adminpassword}")
+    private String password;
 
     Binder<Competition> binder = new Binder<>(Competition.class);
 
@@ -38,39 +51,56 @@ public class CompetitionAdminView extends AbstractTabView {
     private DynamicFileDownloader download = new DynamicFileDownloader("Download CSV", "file.csv", os -> {
         adminService.writeCsvForPirila(binder.getBean(), os);
     });
-    private Button delete = new Button("Delete competition", e-> delete());
+    private Button delete = new Button("Delete competition", e -> delete());
 
     VerticalLayout form = new VerticalLayout(
             new Hr(),
             new H2("Competition details"),
             name, open, start, end,
             new HorizontalLayout(save, raffleRest, download, delete));
-    
+
     ComboBox<Competition> competitionSelector = new ComboBox<>();
     Button createNew = new Button("Create new competition");
 
     @PostConstruct
     void init() {
 
-        createNew.addClickListener(e->newCompetition());
-        save.addClickListener(e->save());
+        createNew.addClickListener(e -> newCompetition());
+        save.addClickListener(e -> save());
 
-    	competitionSelector.setLabel("Select edited competition");
-    	competitionSelector.setItemLabelGenerator(Competition::getName);
+        competitionSelector.setLabel("Select edited competition");
+        competitionSelector.setItemLabelGenerator(Competition::getName);
         listCompetititions();
 
         competitionSelector.addValueChangeListener(e -> setCompetition(e.getValue()));
 
         VHorizontalLayout vHorizontalLayout = new VHorizontalLayout(competitionSelector, createNew);
-    	vHorizontalLayout.setVerticalComponentAlignment(Alignment.END, createNew);
-		add(vHorizontalLayout);
+        vHorizontalLayout.setVerticalComponentAlignment(Alignment.END, createNew);
+        add(vHorizontalLayout);
 
-		form.add(classesAndClassGroupsEditor);
-		form.setVisible(false);
-		binder.bindInstanceFields(this);
-    	add(form);
+        form.add(classesAndClassGroupsEditor);
+        form.setVisible(false);
+        binder.bindInstanceFields(this);
+        add(form);
 
-    	raffleRest.addClickListener(e->raffleRest());
+        raffleRest.addClickListener(e -> raffleRest());
+
+        setVisible(false);
+        Dialog loginDialog = new Dialog();
+        PasswordField pw = new PasswordField();
+        Button login = new Button("Login");
+        login.addClickListener(e -> {
+            Crypt.crypt("sdfsd");
+            if (Crypt.crypt(pw.getValue(), password).equals(password)) {
+                loginDialog.close();
+                setVisible(true);
+            } else {
+                Notification.show("Password did not match! If you want to use the service, contact matti ät tahvonen dot com");
+            }
+        });
+        login.addClickShortcut(Key.ENTER);
+        loginDialog.add(new H1("Login to competition admin"), pw, login);
+        loginDialog.open();
 
     }
 
@@ -102,13 +132,13 @@ public class CompetitionAdminView extends AbstractTabView {
     }
 
     public void newCompetition() {
-    	Competition c = adminService.createNewCompetition();
-    	listCompetititions();
-    	competitionSelector.setValue(c);
+        Competition c = adminService.createNewCompetition();
+        listCompetititions();
+        competitionSelector.setValue(c);
     }
-    
+
     public void setCompetition(Competition c) {
-        if(c != null) {
+        if (c != null) {
             classesAndClassGroupsEditor.setCompetition(c);
             binder.setBean(c);
             form.setVisible(true);
--- src/main/java/org/peimari/starttimeselector/MainView.java
+++ src/main/java/org/peimari/starttimeselector/MainView.java
@@ -1,40 +1,44 @@
 package org.peimari.starttimeselector;
 
-import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.H3;
-import com.vaadin.flow.component.html.Header;
 import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
-import org.peimari.starttimeselector.entities.Competition;
+import com.vaadin.flow.server.VaadinRequest;
 import org.peimari.starttimeselector.entities.Competitor;
 import org.peimari.starttimeselector.entities.SeriesGroup;
 import org.peimari.starttimeselector.entities.StartTime;
 import org.peimari.starttimeselector.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 
 import javax.annotation.PostConstruct;
-import javax.swing.*;
+import java.text.MessageFormat;
 import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
-public class MainView extends AbstractTabView {
+@Route
+public class MainView extends VerticalLayout {
 
     @Autowired
     private UserService userService;
 
-    private H1 header = new H1("Start time selector");
-    private H2 sectionHeader = new H2("Type in your license id and emit number (can be empty if you rent one):");
+    @Autowired
+    ApplicationContext springAppCtx;
 
-    private TextField license = new TextField("License");
-    private TextField emit = new TextField("Emit");
-    private Button login = new Button("Login");
+
+    private H1 header = new H1(translate("mainview.header"));
+    private H2 sectionHeader = new H2(translate("mainview.sectionHeader"));
+
+    private TextField license = new TextField(translate("license"));
+    private TextField emit = new TextField(translate("emit"));
+    private Button login = new Button(translate("login"));
 
     @PostConstruct
     void init() {
@@ -48,7 +52,7 @@ public class MainView extends AbstractTabView {
 
     private void listCompetitions(List<Competitor> competitorInfo) {
         if (competitorInfo.isEmpty()) {
-            Notification.show("No competitions found for your details!", 3000, Notification.Position.MIDDLE);
+            Notification.show(translate("competitions-not-found"), 3000, Notification.Position.MIDDLE);
         } else {
             removeAll();
             add(header);
@@ -72,30 +76,57 @@ public class MainView extends AbstractTabView {
             this.competitor = competitor;
 
             SeriesGroup seriesGroup = competitor.getSeries().getSeriesGroup();
-            add(new H2(seriesGroup.getCompetition().getName()));
+            add(new H2(seriesGroup.getCompetition().getName() + ": " + competitor.getName()));
             if (competitor.getStartTime() == null) {
-                startTimeComboBox = new ComboBox<>("Pick from available start times");
+                startTimeComboBox = new ComboBox<>(translate("pic-start-time"));
                 startTimeComboBox.setItemLabelGenerator(s -> s.getTime().toLocalTime().toString());
                 startTimeComboBox.setItems(userService.findAvailableStartTimes(competitor.getSeries().getSeriesGroup()));
                 startTimeComboBox.addValueChangeListener(e -> {
                     try {
                         userService.reserveStartTime(competitor, e.getValue());
                     } catch (Exception ex) {
-                        Notification.show("Failed to reserve start time, probably somebody " +
-                                "just picked to time before your. Please try again!", 3000, Notification.Position.MIDDLE);
+                        Notification.show(translate("failed-to-reserve"), 3000, Notification.Position.MIDDLE);
                     }
                     login();
                 });
                 add(startTimeComboBox);
             } else {
-                    add(new H3("Your preferred start time is: " + competitor.getStartTime().getTime().toLocalTime()));
-                add(new Button("Choose new", e -> {
+                    add(new H3(MessageFormat.format(translate("preferre-start-time-is.0"), competitor.getStartTime().getTime().toLocalTime())));
+                add(new Button(translate("choose.new"), e -> {
                     userService.releaseStartTime(competitor.getStartTime());
                     login();
+                }));
+                add(new Button(translate("choose.foranoter"), e -> {
+                    MainView.this.init();
                 }));
             }
 
         }
     }
+
+    private ResourceBundle msg;
+
+    private String translate(String code) {
+        try {
+            if(msg == null) {
+                try {
+                    VaadinRequest current = VaadinRequest.getCurrent();
+                    String accept = current.getHeader("Accept-Language");
+                    if(accept != null) {
+                        msg = ResourceBundle.getBundle(
+                                "messages", new Locale(accept.split(";")[0].split(",")[0]));
+
+                    }
+                } catch (Exception e) {
+                    msg = ResourceBundle.getBundle(
+                            "messages", getLocale());
+                }
+            }
+            return msg.getString(code);
+        } catch (Exception e) {
+            return "%"+code;
+        }
+    }
+
 
 }
