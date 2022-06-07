b849f32de340689206d2eca010263239566d5ead
Matti Tahvonen <matti@vaadin.com>
2022-06-07 13:16:37 +0300

    Added a view for loading development time test data

---
+++ src/main/java/org/peimari/starttimeselector/adminview/LoadTestDataView.java
@@ -0,0 +1,38 @@
+package org.peimari.starttimeselector.adminview;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinServlet;
+import org.apache.commons.io.IOUtils;
+import org.peimari.starttimeselector.entities.Competition;
+import org.peimari.starttimeselector.service.AdminService;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+@Route
+public class LoadTestDataView extends VerticalLayout {
+
+    private AdminService adminService;
+    public LoadTestDataView(AdminService adminService) {
+        this.adminService = adminService;
+
+        boolean productionMode = VaadinServlet.getCurrent().getService().getDeploymentConfiguration().isProductionMode();
+        if(!productionMode) {
+            Button button = new Button("Add test competition");
+            button.addClickListener(e -> {
+                Competition newCompetition = adminService.createNewCompetition();
+                try {
+                    InputStream is = new FileInputStream("sample-data/irma-export.csv");
+                    adminService.readInSeriesFromIrmaFile(is, newCompetition);
+                    is = new FileInputStream("sample-data/irma-export.csv");
+                    adminService.readInCompetitorsFromIrmaFile(is, newCompetition);
+                    newCompetition.setOpen(true);
+                    adminService.save(newCompetition);
+                } catch (Exception ex) {throw new RuntimeException(ex);};
+            });
+            add(button);
+        }
+    }
+}
